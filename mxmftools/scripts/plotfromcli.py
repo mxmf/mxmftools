c04608bfdd324b7fb71f1a985650be0bc3739602
mxmf <mxmf521@gmail.com>
2023-02-23 20:08:05 +0800

    修改：     mxmftools/scripts/plotfromcli.py 	修改：     setup.py

--- mxmftools/scripts/plotfromcli.py
+++ mxmftools/scripts/plotfromcli.py
@@ -6,5 +6,5 @@ def plotband_from_cli_str(data, str, fig=None, ax=None):
     kwargs = band.make_context("test", str.split()).params
     kwargs.pop("file")
     kwargs.pop("save")
-    kwargs.pop("readfileformat")
+    kwargs.pop("vaspfileformat")
     plotband(data=data, **kwargs, fig=fig, ax=ax)
