ff441db18026ea5968f5aec26397435f00b97767
mxmf <mxmf521@gmail.com>
2023-02-23 20:32:50 +0800

    修改：     mxmftools/scripts/plotfromcli.py

--- mxmftools/scripts/plotfromcli.py
+++ mxmftools/scripts/plotfromcli.py
@@ -3,6 +3,12 @@ from ..figplot import plotband
 
 
 def plotband_from_cli_str(data, str, fig=None, ax=None):
+    import matplotlib as mpl
+    import mxmftools
+    try:
+        mpl.rc_file(f"~/.config/mxmf/matplotlibrc")
+    except:
+        mpl.rc_file(f"{mxmftools.__path__[0]}/matplotlibrc")
     kwargs = band.make_context("test", str.split()).params
     kwargs.pop("file")
     kwargs.pop("save")
