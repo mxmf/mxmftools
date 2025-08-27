a306768c65d1addea449b6f2ac199a6e76ba35ea
mxmf <2993747433@qq.com>
2025-08-27 15:46:23 +0800

    chore(nix): switch to nixos-unstable and simplify Qt dependencies

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,8 +2,7 @@
   description = "Python development environment with uv";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     helix-erasin = {
       url = "github:erasin/helix";
@@ -16,30 +15,15 @@
       let
         pkgs = import nixpkgs {
           system = system;
-          # config.allowUnfree = true;
-          # cudaSupport = true;
         };
-        pkgs-unstable = import nixpkgs {
-          # pkgs = import nixpkgs {
-          system = system;
-          # config.allowUnfree = true;
-          # cudaSupport = true;
-        };
-
       in
       {
         devShells.default = pkgs.mkShell rec{
           buildInputs = with pkgs; [
-            nix
 
             uv
             basedpyright
             ruff
-
-            # cudatoolkit
-            # cudaPackages.cudatoolkit # CUDA Toolkit für GPU-Unterstützung
-            # cudaPackages.cudnn # CUDA Deep Neural Network library
-            # (pkgs-unstable.linuxPackages.nvidia_x11)
 
 
             #for libstdc++.so.6
@@ -47,35 +31,18 @@
             # for libz.so.1
             zlib
 
-            python3Packages.pyqt5
-            # python3Packages.ase
-            python3Packages.matplotlib
-            # python3Packages.tkinter
-            qt5.qtwayland
-            libsForQt5.qt5.qtbase
-            # tk
-
+            python3Packages.pyqt6
+            qt6.full
 
           ];
 
-
           MPLBACKEND = "QtAgg";
-
-          # QT_PLUGIN_PATH = "${pkgs.qt5.qtbase}/${pkgs.qt5.qtbase.qtPluginPrefix}";
-          # QT_PLUGIN_PATH = "${pkgs.qt5.qtbase}/${pkgs.qt5.qtbase.qtPluginPrefix}";
-          # QT_PLUGIN_PATH = ".venv/lib/python3.10/site-packages/PyQt5/Qt5/plugins";
-
-          # LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
-          # CUDA_PATH = pkgs.cudatoolkit;
           NVIDIA_DRIVERS = "/run/opengl-driver/lib";
           LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (buildInputs ++ [ "/run/opengl-driver" ]);
           EXTRA_LDFLAGS = "-L/lib -L/run/opengl-driver/lib";
 
 
           shellHook = ''
-            export ABACUS_PP_PATH="$HOME/.config/mxmftools/pp/sg15_oncv_upf_2020-02-06"
-            export ABACUS_ORBITAL_PATH="$HOME/.config/mxmftools/orbitals/SG15-Version1p0__StandardOrbitals-Version2p0"
-
             if [ ! -d ".venv" ]; then
               uv venv .venv
             fi
