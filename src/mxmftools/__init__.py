f8f1778f38e46617cd5b383ffc068ff9b003d659
mxmf <2993747433@qq.com>
2025-09-02 18:03:27 +0800

    feat(spinmc): add spinmc CLI module integration and dependency

--- src/mxmftools/__init__.py
+++ src/mxmftools/__init__.py
@@ -3,6 +3,7 @@ import typer
 from .subfig import subfig
 from .vasp.cli import app as vasp_app
 from .wannier90.cli import app as wannier_app
+from spinmc import app as spinmc_app  # type:ignore
 
 app = typer.Typer(
     no_args_is_help=True,
@@ -12,4 +13,5 @@ app = typer.Typer(
 
 app.add_typer(vasp_app, name="vasp")
 app.add_typer(wannier_app, name="wannier90")
+app.add_typer(spinmc_app, name="spinmc")
 _ = app.command()(subfig)
